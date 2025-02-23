af855d3181e955ec6abd0f8c135513b33e681649
Luna <cli-luna@protonmail.com>
2025-02-23 04:02:04 +0100

    Add doc header for glib module.

---
+++ modules/glib/source/nulib/system/glib/package.d
@@ -0,0 +1,10 @@
+/**
+    GLib
+
+    Copyright: Copyright © 1995-1997, Peter Mattis, Spencer Kimball and Josh MacDonald
+    Copyright: Copyright © 2023-2025, Kitsunebi Games
+    Copyright: Copyright © 2023-2025, Inochi2D Project
+    License:   $(LINK2 https://gitlab.gnome.org/GNOME/glib/-/blob/main/LICENSES/LGPL-2.1-or-later.txt, LGPL-2.1-or-later)
+    Authors:   Luna Nielsen
+*/
+module nulib.system.glib;

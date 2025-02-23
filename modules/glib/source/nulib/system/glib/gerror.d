7cfde4caa1939e3440d3fef547884c5e6c8e39bb
Luna <cli-luna@protonmail.com>
2025-02-23 04:01:23 +0100

    Add GQuark, GError and GMutex

---
+++ modules/glib/source/nulib/system/glib/gerror.d
@@ -0,0 +1,256 @@
+/**
+    GLib Errors and Quarks.
+
+    Copyright: Copyright © 1995-1997, Peter Mattis, Spencer Kimball and Josh MacDonald
+    Copyright: Copyright © 2023-2025, Kitsunebi Games
+    Copyright: Copyright © 2023-2025, Inochi2D Project
+    License:   $(LINK2 https://gitlab.gnome.org/GNOME/glib/-/blob/main/LICENSES/LGPL-2.1-or-later.txt, LGPL-2.1-or-later)
+    Authors:   Luna Nielsen
+*/
+module nulib.system.glib.gerror;
+import nulib.string;
+import numem;
+
+enum gIsModuleString(alias sym) = 
+    is(typeof(sym) == inout(char)*) &&
+    !__traits(compiles, { sym = "New value"; });
+
+/**
+    An index into the string table of GLib.
+*/
+struct GQuark {
+@nogc nothrow:
+private:
+    uint g_value = 0;
+
+public:
+    alias toString this;
+    @disable this();
+
+    /**
+        The index value of the quark.
+    */
+    @property uint value() { return g_value; }
+
+    /**
+        The underlying C string for this quark.
+    */
+    @property const(char)* ptr() { return g_quark_to_string(this); }
+    
+    /**
+        Tries to find a quark by its string value.
+
+        Params:
+            value = The string to search for.
+        
+        Returns:
+            A GQuark, check its validity with $(D isValid)
+
+        Note:
+            This function must not be used before library 
+            constructors have finished running.
+    */
+    static GQuark find(inout(char)* value) {
+        return g_quark_try_string(value);
+    }
+
+    /**
+        Creates a GQuark from a null-terminated string.
+    */
+    static GQuark fromString(ref inout(char)* value) {
+        return g_quark_from_string(value);
+    }
+
+    /**
+        Creates a GQuark from a slice based string.
+    */
+    static GQuark fromString(ref inout(char)[] value) {
+        return g_quark_from_string(nstring(value).ptr); // nstring null terminates the string.
+    }
+
+    /**
+        Creates a quark from a static string reference.
+        
+        Static strings can only appear on a module level.
+    */
+    static GQuark fromString(alias staticString)() if (gIsModuleString!staticString) {
+        return g_quark_from_static_string(staticString);
+    }
+
+    /**
+        Gets whether the GQuark contains a valid value.
+
+        Returns:
+            $(D true) if the GQuark contains a value other than 0,
+            $(D false) otherwise.
+    */
+    bool isValid() {
+        return value != 0;
+    }
+
+    /**
+        Gets a string from the GQuark, the GQuark owns the string
+        and it should not be freed.
+    */
+    string toString() {
+        const(char)* text = g_quark_to_string(this);
+        return cast(string)text[0..nu_strlen(text)];
+    }
+}
+
+/**
+    Type of error codes that may be returned by GError.
+*/
+alias GErrorCode = int;
+
+/**
+    A GLib error containing information about errors which have occured.
+
+    Note:
+        GErrors are not reference counted, you will need to manually free
+        them using $(D GError.free).
+*/
+struct GError {
+@nogc nothrow:
+private:
+    GQuark g_domain;
+    GErrorCode g_code;
+    const(char)* g_message;
+
+public:
+    @disable this();
+
+    /**
+        The domain of the error.
+    */
+    @property GQuark domain() { return g_domain; }
+
+    /**
+        The GLib Error Code of the Error.
+    */
+    @property GErrorCode code() { return g_code; }
+
+    /**
+        The error message.
+    */
+    @property string message() { return g_message is null ? null : cast(string)g_message[0..nu_strlen(g_message)]; }
+
+    /**
+        Creates a new GError with the specified format string.
+
+        Params:
+            domain =    The error domain in which to create the error.
+            code =      The $(D GErrorCode) of the error.
+            format =    The format string for the error text.
+            args =      Arguments to pass to the format function.
+        
+        Returns:
+            A newly allocated GError.
+    */
+    static GError* create(Args...)(GQuark domain, GErrorCode code, const(char)* format, Args args) {
+        return g_error_new(domain, code, format, args);
+    }
+
+    /**
+        Frees self.
+    */
+    void free() { g_error_free(&this); }
+
+    /**
+        Checks for equality between GError instances.
+
+        Params:
+            other = the error to compare with
+
+        Returns:
+            $(D true) if other refers to the same kind of error,
+            $(D false) otherwise.
+    */
+    bool opEquals(inout(GError)* other) {
+        return g_error_matches(other, domain, code);
+    }
+
+    /**
+        Wraps the GError into an exception.
+
+        Params:
+            nextInChain =   Next exception in the exception chain, if any.
+            file        =   The file the error occured in, automatically 
+                            filled out by the compiler.
+            line        =   The line on which the exception was created,
+                            automatically filled out by the compiler.
+        
+        Returns:
+            A new GException wrapping a $(B copy) of the GError,
+            the original value is freed by this call.
+            Attempting to access the GError after wrapping it in a
+            GException is undefined behaviour, but probably a crash.
+
+        Examples:
+            ---
+            GError* err;
+            g_thread_try_new(null, (void* value) { return null; }, null, err);
+            if (err)
+                throw err.toException();
+            ---
+    */
+    pragma(inline, true)
+    GException toException(Throwable nextInChain = null, string file = __FILE__, size_t line = __LINE__) {
+        auto g_error_copied = g_error_copy(&this);
+        g_error_free(&this);
+        return assumeNoThrowNoGC(
+            (GError* err, Throwable nextInChain, string file, size_t line) { return nogc_new!GException(err, nextInChain, file, line); },
+            g_error_copied, 
+            nextInChain, 
+            file, 
+            line
+        );
+    }
+}
+
+/**
+    Wrapper for GErrors.
+
+    GExceptions allow for GErrors to be thrown into D code for handling,
+    the GError will automatically be freed when the GException is freed.
+*/
+class GException : NuException {
+@nogc:
+private:
+    GError* g_error;
+
+public:
+
+    /**
+        Constructs a GException from a GError.
+    */
+    this(GError* error, Throwable nextInChain = null, string file = __FILE__, size_t line = __LINE__) {
+        this.g_error = error;
+        super(g_error.message, nextInChain, file, line);
+    }
+
+    /**
+        Frees the GException.
+    */
+    override
+    void free() @trusted {
+        super.free();
+        g_error_free(g_error);
+    }
+}
+
+private:
+
+extern(C) @nogc nothrow:
+
+// GQuark
+extern GQuark g_quark_from_static_string(const const(char)* str);
+extern GQuark g_quark_from_string(const(char)* str);
+extern const(char)* g_quark_to_string(GQuark quark);
+extern GQuark g_quark_try_string(const(char)* str);
+
+// GError
+extern void g_error_free(GError* error);
+extern GError* g_error_copy(const(GError)* error);
+extern GError* g_error_new(GQuark domain, int code, const(char)* format, ...);
+extern bool g_error_matches(const(GError)* error, GQuark domain, int code);
--- modules/glib/source/nulib/system/glib/gthread.d
+++ modules/glib/source/nulib/system/glib/gthread.d
@@ -8,75 +8,121 @@
     Authors:   Luna Nielsen
 */
 module nulib.system.glib.gthread;
+import nulib.system.glib.gerror;
+import numem.core.traits : isSomeFunction;
+import numem.core.hooks : nu_malloc, nu_free;
+import hookset;
 
 /**
     A GLib Thread.
 */
 struct GThread {
-private nothrow:
-@nogc:
-    GThreadT* g_self;
+private:
 
-    this(GThreadT* g_self) { this.g_self = g_self; }
+    // Temporary storage type for GThread state.
+    // D delegates get mangled during thread creation.
+    // this solves that.
+    struct gthread_state_t {
+        void* function(void* ctx) fptr;
+        void* ctxptr;
+    }
 
 public:
-    
-    /* Disabled, since g_self should not be null. */
+@nogc:
     @disable this();
 
     /**
-        Creates and starts a new thread.
+        Tries to spawn a new thread with the given delegate.
     */
-    this(void delegate() dg)  {
-        g_thread_func_t fp = cast(g_thread_func_t)(void* dg) {
-            (*cast(void delegate()*)&dg)();
-            return null;
+    static GThread* run(void* delegate() scope dg) {
+        GError* err;
+        auto self = GThread.tryRun(dg, err);
+        if (err)
+            throw err.toException();
+        
+        return self;
+    }
+
+    /**
+        Attempts to create a new thread.
+    
+        T
+    */
+    static GThread* tryRun(void* delegate() scope dg, ref GError* error) nothrow {
+        gthread_state_t* state = cast(gthread_state_t*)nu_malloc(gthread_state_t.sizeof);
+        state.ctxptr = dg.ptr;
+        state.fptr = cast(void* function(void* ctx))dg.funcptr;
+
+        g_thread_func_t fp = cast(g_thread_func_t)(void* dgIn) {
+            gthread_state_t* state = cast(gthread_state_t*)dgIn;
+            scope(exit) nu_free(state);
+            
+            try {
+                return state.fptr(state.ctxptr);
+            } catch(Exception ex) {
+                nu_fatal(ex.msg);
+                assert(0);
+            }
+            
+            assert(0);
         };
 
-        g_self = g_thread_new(null, fp, cast(void*)&dg);
+        return g_thread_try_new(null, fp, state, error);
     }
 
     /**
         Waits for the thread
     */
-    void* join() { 
-        return g_thread_join(g_self);
+    void* join() {
+        return g_thread_join(&this);
     }
 
     /**
         Gets the calling thread.
+
+        Returns:
+            A GThread
     */
-    static @property GThread self() { return GThread(g_thread_self()); }
+    static @property GThread* self() nothrow { return g_thread_self(); }
     
     /**
         Yields the calling thread.
     */
-    static void yield() { g_thread_yield(); }
+    static void yield() nothrow { g_thread_yield(); }
 
     /**
         Terminates the calling thread.
     */
-    static void terminate() { g_thread_exit(null); }
+    static void terminate() nothrow { g_thread_exit(null); }
+
+    /**
+        Terminates the calling thread with a given value.
+
+        Params:
+            value = The value to pass to $(D join).
+    */
+    static void exit(void* value) nothrow { g_thread_exit(value); }
 }
 
 
-@("Threading")
+@("GThread")
 unittest {
-    int x;
-    GThread t = GThread(() {
+    __gshared int x;
+
+    GThread* t = GThread.run(() {
         x = 42;
+        return cast(void*)x;
     });
+    assert(cast(int)t.join() == x);
 }
 
 private:
 extern(C) nothrow @nogc:
 
-alias g_thread_func_t = extern(C) void* function(void*) @nogc nothrow;
+alias g_thread_func_t = extern(C) void* function(void*) @nogc;
 
-struct GThreadT;
-
-extern GThreadT* g_thread_new(const(char)* name, g_thread_func_t func, void* data);
+extern GThread* g_thread_try_new(const(char)* name, g_thread_func_t func, void* data, ref GError* error);
 extern void g_thread_exit(void* retval);
-extern GThreadT* g_thread_self();
+extern GThread* g_thread_self();
 extern void g_thread_yield();
-extern void* g_thread_join(GThreadT* thread);
+extern void* g_thread_join(GThread* thread);
